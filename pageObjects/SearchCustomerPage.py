09b598b2bb7891531811ecec3dce7911d245ca2c
bsing <bsinghal555@gmail.com>
2023-08-01 18:54:26 +0530

    initial commit

--- pageObjects/SearchCustomerPage.py
+++ pageObjects/SearchCustomerPage.py
@@ -1,3 +1,6 @@
+from selenium.webdriver.common.by import By
+
+
 class SearchCustomer():
     # Add customer Page
     txtEmail_id = "SearchEmail"
@@ -13,31 +16,31 @@ class SearchCustomer():
         self.driver = driver
 
     def setEmail(self,email):
-        self.driver.find_element_by_id(self.txtEmail_id).clear()
-        self.driver.find_element_by_id(self.txtEmail_id).send_keys(email)
+        self.driver.find_element(By.ID, self.txtEmail_id).clear()
+        self.driver.find_element(By.ID, self.txtEmail_id).send_keys(email)
 
     def setFirstName(self,fname):
-        self.driver.find_element_by_id(self.txtFirstName_id).clear()
-        self.driver.find_element_by_id(self.txtFirstName_id).send_keys(fname)
+        self.driver.find_element(By.ID, self.txtFirstName_id).clear()
+        self.driver.find_element(By.ID, self.txtFirstName_id).send_keys(fname)
 
     def setLastName(self,lname):
-        self.driver.find_element_by_id(self.txtLastName_id).clear()
-        self.driver.find_element_by_id(self.txtLastName_id).send_keys(lname)
+        self.driver.find_element(By.ID, self.txtLastName_id).clear()
+        self.driver.find_element(By.ID, self.txtLastName_id).send_keys(lname)
 
     def clickSearch(self):
-        self.driver.find_element_by_id(self.btnSearch_id).click()
+        self.driver.find_element(By.ID, self.btnSearch_id).click()
 
     def getNoOfRows(self):
-        return len(self.driver.find_elements_by_xpath(self.tableRows_xpath))
+        return len(self.driver.find_elements(By.XPATH, self.tableRows_xpath))
 
     def getNoOfColumns(self):
-        return len(self.driver.find_elements_by_xpath(self.tableColumns_xpath))
+        return len(self.driver.find_elements(By.XPATH, self.tableColumns_xpath))
 
     def searchCustomerByEmail(self,email):
         flag=False
         for r in range(1,self.getNoOfRows()+1):
-          table=self.driver.find_element_by_xpath(self.table_xpath)
-          emailid=table.find_element_by_xpath("//table[@id='customers-grid']/tbody/tr["+str(r)+"]/td[2]").text
+          table=self.driver.find_element(By.XPATH, self.table_xpath)
+          emailid=table.find_element(By.XPATH, "//table[@id='customers-grid']/tbody/tr["+str(r)+"]/td[2]").text
           if emailid == email:
               flag = True
               break
@@ -46,8 +49,8 @@ class SearchCustomer():
     def searchCustomerByName(self,Name):
         flag=False
         for r in range(1,self.getNoOfRows()+1):
-          table=self.driver.find_element_by_xpath(self.table_xpath)
-          name=table.find_element_by_xpath("//table[@id='customers-grid']/tbody/tr["+str(r)+"]/td[3]").text
+          table=self.driver.find_element(By.XPATH, self.table_xpath)
+          name=table.find_element(By.XPATH, "//table[@id='customers-grid']/tbody/tr["+str(r)+"]/td[3]").text
           if name == Name:
               flag = True
               break
--- testCases/conftest.py
+++ testCases/conftest.py
@@ -3,6 +3,7 @@ from selenium import webdriver
 
 @pytest.fixture()
 def setup(browser):
+
     if browser=='chrome':
         driver=webdriver.Chrome()
         print("Launching chrome browser.........")
@@ -18,13 +19,13 @@ def pytest_addoption(parser):    # This will get the value from CLI /hooks
 def browser(request):  # This will return the Browser value to setup method
     return request.config.getoption("--browser")
 
-########### pytest HTML Report ################
-
-# It is hook for Adding Environment info to HTML Report
-def pytest_configure(config):
-    config._metadata['Project Name'] = 'nop Commerce'
-    config._metadata['Module Name'] = 'Customers'
-    config._metadata['Tester'] = 'Pavan'
+# ########### pytest HTML Report ################
+#
+# # It is hook for Adding Environment info to HTML Report
+# def pytest_configure(config):
+#     config._metadata['Project Name'] = 'nop Commerce'
+#     config._metadata['Module Name'] = 'Customers'
+#     config._metadata['Tester'] = 'Pavan'
 
 # It is hook for delete/Modify Environment info to HTML Report
 @pytest.mark.optionalhook
