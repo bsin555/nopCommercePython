78f812c12167e2c5114a8cd27baf38ec69405cc0
bsing <bsinghal555@gmail.com>
2023-08-01 19:06:07 +0530

    run.bat file changes

--- run.bat
+++ run.bat
@@ -1,1 +1,3 @@
-pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
+pytest -s -v --html=./Reports/report.html testCases/test_login.py --browser chrome
+
+REM: pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
