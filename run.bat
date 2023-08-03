b05557d0766804d1416fb3c7f0bd24806c1ef061
bsin555 <109945420+bsin555@users.noreply.github.com>
2023-08-03 20:08:18 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
-pytest -s -v --html=./Reports/report.html testCases/test_login.py --browser chrome
+pytest -s -v -m "sanity" --html=./Reports/report.html testCases/test_login.py --browser chrome
 
 REM: pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
