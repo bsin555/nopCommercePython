c832333d0f224bf026a599916114f2934db00fa9
bsin555 <109945420+bsin555@users.noreply.github.com>
2023-08-03 20:15:34 +0530

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,3 +1,5 @@
+pip3 --install --upgrade pip
+pip3 install pytest
 pytest -s -v -m "sanity" --html=./Reports/report.html testCases/test_login.py --browser chrome
 
 REM: pytest -s -v -m "sanity" --html=./Reports/report.html testCases/ --browser chrome
